0d41dac68e93355b7b4563f23e08882e1df2b07c
MolinaHVL <christopher.molina@cetys.edu.mx>
2023-04-13 15:01:41 -0700

    .
    .

--- front/src/pages/index.jsx
+++ front/src/pages/index.jsx
@@ -1,15 +1,34 @@
-import { Stack } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import Box from '@mui/material/Box'
 import ParameterBox from '../components/ParameterBox';
+import { getProducts } from "../api/indexAPI";
+import { useEffect, useState } from "react";
 
-const index = () => {
+
+const Index = () => {
+
+    const [allProducts, setAllProducts] = useState([]);
+
+    useEffect(() => {
+        getAllProducts();
+    }, []);
+
+    const getAllProducts = async () => {
+        const products = await getProducts();
+        setAllProducts(products);
+    };
+
+    if (allProducts.length === 0) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <Box
             sx={{
                 display: "flex",
                 minWidth: "80vw",
                 minHeight: "80vh",
-                margin: "10%"
+                margin: "10%",
             }}
         >
             <Box
@@ -19,22 +38,23 @@ const index = () => {
                     opacity: 0.6,
                     minWidth: "100%",
                     minHeight: "100%",
-                }}>
+                }}
+            >
                 <Stack direction={"column"} sx={{ flexGrow: 1 }}>
                     <Stack direction={"row"} display={"flex"} sx={{ flexGrow: 1 }}>
-                        <ParameterBox title={"temperatura"} value={17} sx={{ display: "flex", flexGrow: 1 }} />
-                        <ParameterBox title={"temperatura"} value={17} sx={{ flexGrow: 1 }} />
-                        <ParameterBox title={"temperatura"} value={17} sx={{ flexGrow: 1 }} />
+                        <ParameterBox title={"temperatura"} value={allProducts[allProducts.length - 1].temperature} />
+                        <ParameterBox title={"temperatura"} value={17} />
+                        <ParameterBox title={"temperatura"} value={17} />
                     </Stack>
                     <Stack direction={"row"} display={"flex"} sx={{ flexGrow: 1 }}>
-                        <ParameterBox title={"temperatura"} value={17} display={"flex"} sx={{ flexGrow: 1 }} />
-                        <ParameterBox title={"temperatura"} value={17} display={"flex"} sx={{ flexGrow: 1 }} />
-                        <ParameterBox title={"temperatura"} value={17} display={"flex"} sx={{ flexGrow: 1 }} />
+                        <ParameterBox title={"temperatura"} value={17} />
+                        <ParameterBox title={"temperatura"} value={17} />
+                        <ParameterBox title={"temperatura"} value={17} />
                     </Stack>
                 </Stack>
             </Box>
         </Box>
     );
-}
+};
 
-export default index;
+export default Index;
